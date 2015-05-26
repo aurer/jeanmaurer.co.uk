94504535443634c7689e14c0dfb6c720eee616b4
Phil Maurer <philmau@gmail.com>
2015-05-26 23:30:13 +0100

    Adding a snippet to customise display of 'about' content

---
+++ site/snippets/about.php
@@ -0,0 +1,8 @@
+<div class="grid grid--pad">
+	<div class="col-lg-1of2">
+		<img src="http://lorempixel.com/600/400/" alt="">
+	</div>
+	<div class="col-lg-1of2">
+		<?php echo $section->text()->kirbytext() ?>
+	</div>
+</div>
