98543495bdaa3148ed7cac4157b8ed13c5c9c5a6
Phil Maurer <philmau@gmail.com>
2015-06-07 20:53:13 +0100

    Improving gallery thumbnails responsive sizing

--- site/snippets/gallery-image.php
+++ site/snippets/gallery-image.php
@@ -1,4 +1,4 @@
-<div class="col-1of4">
+<div class="col-md-1of2 col-xl-1of4">
 	<div class="gallery-image">
 		<a href="<?php echo $image->url() ?>">
 			<?php echo thumb($image, c::get('thumb_options')) ?>
