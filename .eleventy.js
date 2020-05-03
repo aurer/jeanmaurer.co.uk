b1887ae01bef65a677b10fea886f74a633658a7a
Phil Maurer <philmau@gmail.com>
2020-05-03 22:32:28 +0100

    Remove reference to clean-css

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,4 @@
-const CleanCSS = require('clean-css');
 module.exports = function (eleventyConfig) {
-	eleventyConfig.addFilter('cssmin', (code) => new CleanCSS({}).minify(code).styles);
 	eleventyConfig.addWatchTarget('./site/style.css');
 	eleventyConfig.setTemplateFormats(['md', 'pug', 'jpg', 'png']);
 	return {
